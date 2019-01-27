2c1fc6959900109390ba31a4150e48b6cc2d1c90
LPjavier <42053940+lugof@users.noreply.github.com>
2019-01-26 22:04:30 -0600

    Updates

---
+++ precache-manifest.33448f4a52175d898735894747f8e5a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44af4bdf6513f12330d8",
+    "url": "/clicky-Francisco/static/js/runtime~main.44af4bdf.js"
+  },
+  {
+    "revision": "3457d181bc0fa8ec5601",
+    "url": "/clicky-Francisco/static/js/main.3457d181.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-Francisco/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3457d181bc0fa8ec5601",
+    "url": "/clicky-Francisco/static/css/main.1c0a077f.chunk.css"
+  },
+  {
+    "revision": "641fa78030dc6c8b0df7cf35a2a6eb19",
+    "url": "/clicky-Francisco/index.html"
+  }
+];
